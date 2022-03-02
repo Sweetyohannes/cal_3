4e0a5031747ca8dfb9ed3e6b7e4178dd72f9b689
sweet <sweetyohannes.jb@gmail.com>
2022-03-02 07:39:04 -0800

    we are making the testing

--- calculator_test.py
+++ calculator_test.py
@@ -8,6 +8,22 @@ class Test(unittest.TestCase):
     def test_0_add(self):
         result = self.calculator.add(4,8)
         self.assertEqual(result,12)
+class Test1(unittest.TestCase):
+    calculator = CalculatorClass.Calculator() # instantiate the Calculator Class
+    def test_0_add(self):
+        result1 = self.calculator.subtract(10,8)
+        self.assertEqual(result1,2)
+class Test2(unittest.TestCase):
+    calculator = CalculatorClass.Calculator() # instantiate the Calculator Class
+    def test_0_add(self):        
+        result2 = self.calculator.multiply(2,8)
+        self.assertEqual(result2,16)
+class Test3(unittest.TestCase):
+    calculator = CalculatorClass.Calculator() # instantiate the Calculator Class
+    def test_0_add(self):        
+        result3 = self.calculator.divide(8,4)
+        self.assertEqual(result3,2)
+        
 if __name__ == '__main__':
     # begin the unittest.main()
     unittest.main()
